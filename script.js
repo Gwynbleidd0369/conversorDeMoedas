d9bcf75373cdea8a546f9b3c9e48a4c539e3bf4c
danilo melo <danilomarquesdemelo@gmail.com>
2022-08-07 21:18:10 -0300

    Implantado HTML e CSS

---
+++ script.js
@@ -0,0 +1,10 @@
+// const url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         const tela = document.querySelector('.result')
+//         const p = document.createElement('p')
+//         p.innerHTML = data.USDBRL.ask
+//         tela.appendChild(p)
+//     })
